29dd90d2cfc5042b8a5b0fc22736554fc0a65c1c
nez1001 <haniz.hassaram@gmail.com>
2024-07-30 15:33:59 +0700

    frist comment

--- Lab 5.py
+++ Lab 5.py
@@ -4,3 +4,9 @@ print("name : Hanis Hasram")
 git config -–global user.name “nez1001”
 
 git config --global user.gmail "haniz.hassaram@gmail.com" 
+
+git remote add Lab.5 https://github.com/nez1001/Lab.5.git
+
+git push –-set-upstream Lab.5 master
+
+git remote add origin https://github.com/nez1001/Lab.5.git
